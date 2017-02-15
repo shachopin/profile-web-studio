6c8215719fb9fb2bf557ee1c013350e283a97128
Dawei Dai <shachopin@gmail.com>
2017-02-14 16:07:30 -0800

    add sinatra rest service url

--- js/work.js
+++ js/work.js
@@ -13,18 +13,19 @@ var projects2 = [
   {
     pic: "https://www.petfinder.com/wp-content/uploads/2012/09/Blog-Kitty-Cam-solo.jpg",
     title: "Project 1",
-    description: "Node Project",
+    description: "Node Todo List",
     link: "https://oracle-jet.gomix.me/"
   }, {
     pic: "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg",
     title: "Project 2",
-    description: "Sinatra Project",
+    description: "Sinatra Blackjack Game",
     link: "http://david-blackjack.herokuapp.com"
   }, {
     pic: "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg",
     title: "Project 3",
-    description: "Ruby on Rails Project",
-    link: "#"
+    //description: "Ruby on Rails Project",
+    description: "Sinatra Rest Service",
+    link: "https://calling-uno-service.gomix.me"
   }, {
     pic: "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg",
     title: "Project 4",
